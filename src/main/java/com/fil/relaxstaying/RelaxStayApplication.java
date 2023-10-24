bfda84a81b87db3c5bd6fd93d396cf5a5ece3f54
arshdeepdubey.ad <53818463+Arshdeepdubey@users.noreply.github.com>
2023-10-24 16:33:49 +0530

    Add files via upload

---
+++ src/main/java/com/fil/relaxstaying/RelaxStayApplication.java
@@ -0,0 +1,13 @@
+package com.fil.relaxstaying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaxStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelaxStayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fil/relaxstaying/RelaxStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fil.relaxstaying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelaxStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
